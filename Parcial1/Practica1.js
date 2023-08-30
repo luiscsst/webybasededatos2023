bab7b47e2ee2e8806b5d95c3f4f41561668af749
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-08-29 19:31:49 -0700

    29/08/23

---
+++ Parcial1/Practica1.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+
+    $("#boton1"). on("click", function(){
+      $("#texto1").html("Texto agregado al parrafo")
+    });
+
+    $("#texto1").hover(function(){
+    $(this).css("background-color", "purple");
+    $(this).css("font-size", "35px");
+    }, function() {
+        $(this).css("background-color", "blue");
+        $(this).css("font-size", "35px");
+        $("#boton2").click(function (e){
+            $("imagen1").fadeToggle("slow");
+    
+        });
+    }
+    );
+   
+}) ;

e3e29451365b6f9e52f07e86cd6f290c7b051c27
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-09-11 19:19:40 -0700

    11/09/23

--- Parcial1/pares.js
+++ Parcial1/pares.js
@@ -1,33 +1,59 @@
-$(document).ready(function (){
-    var par1="";
+$(document).ready(function(){
+    var carta1="";
     var par=false;
-    var par2="";
+    var carta2="";
     var total_pares=0;
     $('img').click(function(e){
         var estado= $(this).attr('data-estado')
         var nombre_imagen= $(this).attr('data-id')
+
         if(estado=="0"){
-            console.log("estado de la carta: " + estado)
-            if(par1==""){
-                par1=$(this);
-                par1.attr('src', 'imagenes/' + nombre_imagen)
-                console.log("se asigno par #1");
-            }else{
-                par1=$(this);
-                par1.attr('src', 'imagenes/' + nombre_imagen)
-                par1.attr('src', 'imagenes/' + nombre_imagen)
-                console.log("se asigno par #2");
+            if(carta1!= ""&& carta2!= ""){
+                console.log("Las cartas son diferentes")
+                if(carta1.attr('data-id')!=carta2.attr('data-id')){
+                    console.log("Tapa cartas diferentes")
+                    carta1.attr('src','imagenes/tapar.jpg')
+                    carta2.attr('src','imagenes/tapar.jpg')
+                }else{
+                    console.log("Las cartas son pares")
+                    carta1.attr('data-estado','1')
+                    carta2.attr('data-estado','1')
+                    par=false
+                }
+                carta1=""
+                carta2=""
             }
         }
-
-        if(par1!="" && par2 != ""){
-            if(par1.attr('data-id')==par2.attr('data-id')){
-                par = true
+        if(estado=="0"){
+            console.log("Estado de la carta: " + estado)
+            if(carta1==""){
+                carta1=$(this);
+                carta1.attr('src','imagenes/' + nombre_imagen)
+                console.log("Se asigno carta #1");
+            }else{
+                carta2=$(this);
+                carta2.attr('src','imagenes/' + nombre_imagen)
+                console.log("Se asigno carta #2");
+            }
+        }
+        if(carta1 !="" && carta2!=""){
+             if(carta1.attr('data-id')==carta2.attr('data-id')){
+                par=true
                 total_pares++
                 $("#total_pares").html(total_pares)
-            }
+                carta1="";
+                carta2="";
+             }
         }
-
-        
+        if(total_pares==8){
+            $('img').each(function() {
+                $(this).attr('src','imagenes/tapar.jpg')
+            });
+            total_pares=0
+            par=false
+            carta1=""
+            carta2=""
+            return;
+        }
     });
 });

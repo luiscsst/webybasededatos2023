cbb46106cf3ffbc828f3284ba0a6f41f6f2658f6
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-09-13 19:25:35 -0700

    13/09/23

---
+++ Parcial1/Practica4.js
@@ -0,0 +1,11 @@
+$(document).ready(function ()  {
+var valor_dado = 0;
+$("#boton").click(function (e) {
+    valor_dado = Math.floor(Math.random()*6+1)
+    console.log("Sacaste  un: " + valor_dado)
+    $("#dado").attr("src", "imagenes/d" + valor_dado + ".png");
+    $("#numero").html(valor_dado)
+
+});
+});
+

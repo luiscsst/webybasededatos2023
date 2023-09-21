7dff740b03569150264618f03565d9d1bb88ea2c
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-09-20 21:08:11 -0600

    Add files via upload

--- Parcial1/proyecto1.js
+++ Parcial1/proyecto1.js
@@ -1,11 +1,27 @@
-$(document).ready(function ()  {
-var valor_dado = 0;
-$("#boton").click(function (e) {
-    valor_dado = Math.floor(Math.random()*10+1)
-    console.log("Sacaste  un: " + valor_dado)
-    $("#dado").attr("src", "imagenesproyecto/carta" + valor_dado + ".PNG");
-    $("#numero").html(valor_dado)
+$(document).ready(function () {
+    var valor_dado1 = 0;
+    var total1 = 0;
+    
+    var valor_dado2 = 0;
+    var total2 = 0;
 
-});
-});
+    $("#boton1").click(function () {
+        valor_dado1 = Math.floor(Math.random() * 10 + 1);
+        console.log("Jugador 1 sacó un: " + valor_dado1);
+        $("#dado1").attr("src", "imagenesproyecto/carta" + valor_dado1 + ".PNG");
+        $("#numero1").html(valor_dado1);
 
+        total1 += valor_dado1;
+        $("#total1").html("Total: " + total1);
+    });
+
+    $("#boton2").click(function () {
+        valor_dado2 = Math.floor(Math.random() * 10 + 1);
+        console.log("Jugador 2 sacó un: " + valor_dado2);
+        $("#dado2").attr("src", "imagenesproyecto/carta" + valor_dado2 + ".PNG");
+        $("#numero2").html(valor_dado2);
+
+        total2 += valor_dado2;
+        $("#total2").html("Total: " + total2);
+    });
+});

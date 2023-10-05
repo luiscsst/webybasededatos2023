34d33d4cd4ccc77d92d03531d21a271bb6821226
luiscsst <124744464+luiscsst@users.noreply.github.com>
2023-10-04 19:28:25 -0700

    04/10/23

---
+++ Parcial1/Parcial2/conexion.php
@@ -0,0 +1,12 @@
+    <?php 
+        $server = "localhost";
+        $user = "root";
+        $password = ""; 
+
+
+        $conexion = new mysqli($server, $user, $password);
+
+        if($conexion->connect_error){
+           die("fallo la conexion". $conexion->connect_error); 
+        }
+    ?>
